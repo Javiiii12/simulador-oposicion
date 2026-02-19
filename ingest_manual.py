e3ef2a2f90f37aa523b36c15ac9f1b2a60ba28d8
Javier Martínez Valiente <javier_martinez92@hotmail.com>
2026-02-19 01:25:14 +0100

    Actualización de test masiva (Auto-Sync)

--- ingest_manual.py
+++ ingest_manual.py
@@ -19,8 +19,8 @@ def ingest_manual_data():
     # Solution: "Solución: x"
     re_sol = re.compile(r'Solución:\s*([a-d])', re.IGNORECASE)
 
-    # Force everything to Tema 4 as requested by User
-    current_tema = "Tema 4"
+    # Force everything to Tema 5 as requested by User
+    current_tema = "Tema 5"
     current_q = None
     questions = []
     
